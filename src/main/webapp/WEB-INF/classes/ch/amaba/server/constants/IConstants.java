3657e3f65f9c3efcb48ec4d9c95ea95f449f09f2
Rodolphe Gomes <rodolphe.gomes@gmail.com>
2012-03-12 22:14:21 +0100

    untrack files

--- src/main/webapp/WEB-INF/classes/ch/amaba/server/constants/IConstants.java
+++
@@ -1,7 +0,0 @@
-package ch.amaba.server.constants;
-
-public interface IConstants {
-
-	/** Constante variable de session UserCriteria. */
-	static final String SESSION_USER_CRITERIA = "SESSION_USER_CRITERIA";
-}
